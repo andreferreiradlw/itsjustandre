68144b9a295b5e16ee8ebeb1ed54f3c94473167e
Ferreira <ferreiraa@delawareconsulting.com>
2019-02-06 15:07:52 +0000

    Repositories Component + Styling

--- src/app/repositories/repositories.component.ts
+++ src/app/repositories/repositories.component.ts
@@ -21,7 +21,7 @@ export class RepositoriesComponent implements OnInit {
     this.reposSub = this.githubService.getReposUpdateListener()
       .subscribe( repoData => {
         this.repositoryCollection = repoData.sort((val1: any, val2: any) => {
-          return new Date(val1.created_at) - new Date(val2.created_at);
+          return new Date(val2.created_at) - new Date(val1.created_at);
         });
         // console.log(this.repositoryCollection);
       });
